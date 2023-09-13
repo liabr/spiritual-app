0b9e6716ffc52d05d51c459f1025be968fe61637
Lijo Abraham <lijo.abraham.work@gmail.com>
2023-09-13 13:26:25 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,44 @@
-import * as React from "react";
-import { NameEditComponent, ColorBrowser, ColorPicker } from "./components";
-import { Color } from "./models/color";
-import {Accordion} from "./components/HomePage";
+import { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-export const App = () => {
-  const [name, setName] = React.useState("defaultUserName");
-  const [editingName, setEditingName] = React.useState("defaultUserName");
-  const [color, setColor] = React.useState<Color>({
-    red: 90,
-    green: 40,
-    blue: 180
-  });
+import AddTutorial from "./components/add-tutorial.component";
+import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorial-list.component";
 
-  const loadUsername = () => {
-    setTimeout(() => {
-      setName("name from async call");
-      setEditingName("name from async call");
-    }, 500);
-  };
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/tutorials"} className="navbar-brand">
+            bezKoder
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
 
-  React.useEffect(() => {
-    loadUsername();
-  }, []);
-
-  const setUsernameState = () => {
-    setName(editingName);
-  };
-
-  return (
-    <>      
-      <ColorBrowser color={color} onColorUpdated={setColor}  />
-      <ColorPicker color={color} onColorUpdated={setColor}/>
-      <NameEditComponent 
-      initialUserName={name}
-      editingName={editingName}
-      onEditingUpdated={setEditingName}
-      onNameUpdated={setUsernameState}
-      disabled={editingName === ""|| editingName === name}/>
-      <Accordion></Accordion>
-    </>
-  );
-};
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+            <Route exact path="/add" component={AddTutorial} />
+            <Route path="/tutorials/:id" component={Tutorial} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
